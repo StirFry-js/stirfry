add96bdae39d4a41bb4d1069ae776387aff4536b
squishybanana <squishybanana04@gmail.com>
2016-05-18 10:15:22 +0300

    Changes to be committed: 	modified:   testServer.js

--- testServer.js
+++ testServer.js
@@ -1,21 +1,4 @@
 var StirFry = require('./index');
 var server  = new StirFry(8080, '0.0.0.0');
-var users   = [
-	'asdfghjkl;',
-	"1234567890",
-	"1q2w3e4r5t",
-	"pqowieuryt"
-]
-
-server.get(/\/(user|account)\/(.{10}?)/, function (req, res) {
-	console.log(req.url);
-	console.log(req.params[1]);
-	if (users.indexOf(req.params[1]) != -1) {
-		res.send("Hello " + req.params[1]);
-	}
-	else {
-		res.send("user does not exist");
-	}
-});
-
+server.pre(StirFry.static());
 server.listen();
