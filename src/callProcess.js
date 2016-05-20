732bbb0403657a75f1bd9d8493a7a634237816b8
squishybanana <squishybanana04@gmail.com>
2016-05-20 15:09:42 +0300

    dont delete req.params

--- src/callProcess.js
+++ src/callProcess.js
@@ -9,9 +9,11 @@ StirFry.prototype._callProcessors = function(req, res, asynchronous) {
 			//If the regex matches where i add ^ to the begginning and $ to the end
 			if (RegExp('^' + this.listens['processor'][i].options.url.source + "$").test(req.url)) {
 				//Call it with the request parameters as an array
-				req.params = RegExp('^' + this.listens['processor'][i].options.url.source + "$").exec(req.url).slice(1);
+				var params = RegExp('^' + this.listens['processor'][i].options.url.source + "$").exec(req.url).slice(1);
+				//Loop through params and set req.params[i] to equal params[i]
+				for (var i in params) req.params[i] = params[i];
 				this.listens['processor'][i].call(req, res, end, asynchronous);
-				delete req.params;
+				for (var i in params) delete req.params[i];
 				if (ending) {
 					break;
 				}
