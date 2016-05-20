fe60d20db962299ab985a64222453bf9604425d3
squishybanana <squishybanana04@gmail.com>
2016-05-20 18:35:04 +0300

    request instead of get

--- src/callGets.js
+++ src/callGets.js
@@ -4,19 +4,19 @@ function end() {
 }
 
 //Function to call all the get request
-StirFry.prototype._callGets = function(req, res, asynchronous) {
+StirFry.prototype._callRequests = function(req, res, asynchronous) {
 	ending = false;
 	//Loop through all the gets
-	for (var i = 0; i < this.listens['get'].length; i++) {
+	for (var i = 0; i < this.listens['request'].length; i++) {
 		//If its a regex
-		if (this.listens['get'][i].options.regex) {
+		if (this.listens['request'][i].options.regex) {
 			//If the regex matches where i add ^ to the begginning and $ to the end
-			if (RegExp('^' + this.listens['get'][i].options.url.source + "$").test(req.url)) {
+			if (RegExp('^' + this.listens['request'][i].options.url.source + "$").test(req.url)) {
 				//Call it with the request parameters as an array
-				var params = RegExp('^' + this.listens['get'][i].options.url.source + "$").exec(req.url).slice(1);
+				var params = RegExp('^' + this.listens['request'][i].options.url.source + "$").exec(req.url).slice(1);
 				//Loop through params and set req.params[i] to equal params[i]
 				for (var i in params) req.params[i] = params[i];
-				this.listens['get'][i].call(req, res, end, asynchronous);
+				this.listens['request'][i].call(req, res, end, asynchronous);
 				for (var i in params) delete req.params[i];
 				if (ending) {
 					break;
@@ -24,9 +24,9 @@ StirFry.prototype._callGets = function(req, res, asynchronous) {
 			}
 		}
 		//Else if it is the same
-		else if (this.listens['get'][i].options.url == req.url) {
+		else if (this.listens['request'][i].options.url == req.url) {
 
-			this.listens['get'][i].call(req, res, end, asynchronous);
+			this.listens['request'][i].call(req, res, end, asynchronous);
 
 			if (ending) {
 				break;
