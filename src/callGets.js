ce41349b5794e429e00fefc7f3a3badf88dd95ab
squishybanana <squishybanana04@gmail.com>
2016-05-26 16:39:03 +0300

    Fixed bug where not responding

--- src/callGets.js
+++ src/callGets.js
@@ -27,11 +27,11 @@ StirFry.prototype._callRequests = function(req, res, asynchronous) {
 		//Else if it is the same
 		else {
 			var keys = [];
-			var params = (pathToRegexp(this.listens['request'][i].options.url, keys).exec(req.url) || []).slice(1);
+			var params = pathToRegexp(this.listens['request'][i].options.url, keys).exec(req.url);
 			console.log(params, req.url);
 			console.log(keys);
-			if (params.length >= 1) {
-
+			if (params) {
+ 				params = params.slice(1)
 				//Loop through params and set req.params[i] to equal params[i]
 				for (var i in params) req.params[keys[i].name] = params[i];
 				console.log(req.params);
