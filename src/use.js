695f7ad8c4d169caa66526b450eeddf8612c74b5
squishybanana <squishybanana04@gmail.com>
2016-06-09 12:19:25 +0300

    new plugin system

--- src/use.js
+++ src/use.js
@@ -1,5 +1,5 @@
 StirFry.prototype.use = function (obj) {
-	if (obj instanceof StirFry) {
+	if (obj.listens) {
 		//Add all its listeners
 		for (var i in obj.listens) {
 			for (var k in obj.listens[i]) {
@@ -7,19 +7,5 @@ StirFry.prototype.use = function (obj) {
 			}
 		}
 		return;
-	}
-	//If the object is an array
-	if (Array.isArray(obj)) {
-		//Run this function on each of the inner ones
-		for (var i = 0; i < obj.length; i++) {
-			this.use(obj[i]);
-		}
-	}
-	else {
-		//If there is a url parameter
-		if (obj.url)
-			this[obj.layer](obj.url, obj.call);
-		else
-			this[obj.layer](obj.call);
 	}
 }
