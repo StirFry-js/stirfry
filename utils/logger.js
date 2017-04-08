2407c08d3dd12c7ee695966c0ad38807a45e7f24
Alex Waese-Perlman <squishybanana@gmail.com>
2017-04-07 21:11:52 -0400

    Follow styles better and fix

--- utils/logger.js
+++ utils/logger.js
@@ -1,13 +1,15 @@
 /*eslint no-console: ["error", { allow: ["log"] }] */
 'use strict';
-let StirFry = require('../stirfry.js');
-let fs = require('fs');
-let formatDate = require('./formatdate');
+const StirFry = require('../stirfry.js');
+const fs = require('fs');
+const formatDate = require('./formatdate');
 
 module.exports = function(path) {
-	let extension = new StirFry.extension();
+	const extension = StirFry.extension();
+
 	extension.req(function(request) {
-		let log = `Request recieved with ${request.post ? `${JSON.stringify(request.post)} as post and ` : ''} ${request.fullUrl || request.url} as the url. Recieved from ${request.ip} on ` + formatDate(new Date());
+		const log = `Request recieved with ${request.post ? `${JSON.stringify(request.post)} as post and ` : ''} ${request.fullUrl || request.url} as the url. Recieved from ${request.ip} on ` + formatDate(new Date());
+		
 		console.log(log);
 		if (path) {
 			fs.appendFile(path, log + '\n');
